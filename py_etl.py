e7ed2088fe2e2307d3f9c4c1f983ed0c33b9b84f
juanjo <juanjomonte@gmail.com>
2025-02-22 15:10:09 +0100

    Wrote solar_indicators_with_solar and solar_indicators_no_solar tables into database

--- py_etl.py
+++ py_etl.py
@@ -112,6 +112,10 @@ solar_indicators_no_solar = df_solar_indicators_raw[df_solar_indicators_raw['has
 
 # Loading the two df into MySQL Workbench
 
+write_to_database(creds=creds['mysql-db'], df=solar_indicators_with_solar, table_name='solar_indicators_with_solar', if_exists='replace')
+
+write_to_database(creds=creds['mysql-db'], df=solar_indicators_no_solar, table_name='solar_indicators_no_solar', if_exists='replace')
+
 
 
 
